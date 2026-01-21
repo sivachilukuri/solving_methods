ada71abc191a975abd28029382aa28ddc8a8fdff
sivachilukuri <sivachilukuriat@gmail.com>
2026-01-21 22:44:09 +0530

    add some problems in problems.py

--- Neetcode/problems.py
+++ Neetcode/problems.py
@@ -143,3 +143,67 @@ def maxArea(height):
 
 #execution
 print(maxArea([1,8,2,3,4,5,6,8,4,6,7]))
+
+# -----------------------------------------
+# Valid Parentheses: 20
+def isvalid(s):
+    mapping = {")":"(","}":"{","]":"["}
+    stack = []
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else "#"
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+#execution
+print(isvalid("{]"))
+
+# -------------------------------------------------------------
+# Min Stack
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            min_val = self.min_stack[-1] 
+            self.min_stack.append(min(min_val, val))
+    def pop(self):
+        self.stack.pop()
+        self.min_stack.pop()
+    def top(self):
+        return self.stack[-1]
+    def get_min(self):
+        return self.min_stack[-1]
+
+# execution
+m = MinStack()
+m.push(2)
+m.push(1)
+print(m.get_min())
+m.pop()
+print(m.top())
+print(m.get_min())
+
+# Daily Temperatures (LeetCode #739)
+# def daily_temperatures(temp):
+#     n = len(temp)
+#     answer = []*n
+#     stack = []
+#     for i in range(len(temp)):
+#         curr_temp = temp[i]
+#         while stack and curr_temp > temp[stack[-1]]:
+#             prev_index = stack.pop()
+#             answer[prev_index] = i - prev_index
+#         stack.append(i)
+#     return answer
+
+# execution
+# p = daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73])
+# print(p)
